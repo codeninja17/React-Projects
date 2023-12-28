ab16f8506e3053ccc4cd79033753570bdccef4df
codeninja17 <ankitkr09@gmail.com>
2023-12-28 10:12:32 +0530

    React State Management

---
+++ steps/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() =>
+            step > 1 ? setStep((currentStep) => currentStep - 1) : step
+          }
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() =>
+            step < 3 ? setStep((currentStep) => currentStep + 1) : step
+          }
+        >
+          Next
+        </button>
+
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => alert(step)}
+        >
+          State
+        </button>
+      </div>
+    </div>
+  );
+}
